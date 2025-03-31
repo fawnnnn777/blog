dea19fb4477982b72b5840799abfed3e4c49f26d
Diego <ale.rgdap@gmail.com>
2025-03-30 18:09:56 -0600

    working on mobile styling

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
-import { FaFacebook } from "react-icons/fa";
+import { RxHamburgerMenu } from "react-icons/rx";
 import { FaSquareInstagram } from "react-icons/fa6";
 import { ImMan } from "react-icons/im";
 
 
 export default function NavBar(){
     return (
-        <div className="w-full border-b p-2 border-black/20 flex justify-between items-center h-[70px]">
-            <div className="flex justify-between"><a href="https://www.instagram.com/_diego_again/"> <FaSquareInstagram className="m-1" size={24}/></a>
+        <div className="w-screen left-0 sm:w-full border-b p-2 border-black/20 flex justify-between items-center h-[70px]">
+            <div className="flex justify-between"><a className="hidden sm:block" href="https://www.instagram.com/_diego_again/"> <FaSquareInstagram className="m-1" size={24}/></a>
+            <RxHamburgerMenu size={24} className="block sm:hidden m-1"/>
             </div>
             <div className="flex justify-between items-center"><p className="m-1 text-2xl text-textColor">Basic Being Basic</p> <ImMan className="m-1" size={32} /></div>
             <div><Link href="/SignIn" className="box-border rounded-3xl p-2 m-1 border border-black/20 font-bold text-sm
